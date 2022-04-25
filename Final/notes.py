1835348be3e4a20525abe74902473493cf737d3d
Xiangan He <xiangan@polychain.capital>
2022-04-25 14:44:10 -0400

    final proj notes

--- Final/notes.py
+++ Final/notes.py
@@ -5,12 +5,28 @@ import pygame
 pygame.init()
 
 scene = pygame.display.set_mode((1400,800))
+pygame.display.set_caption("My new caption!")
 
-while True:
+pygame.mixer.music.load('music.mp3')
+
+pygame.mixer.music.play()
+
+running = True
+
+logo = pygame.image.load('logo.png')
+logo_frame = logo.get_rect()
+
+while running:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            running = False
+
     
     scene.fill([0,153,0])
+    scene.blit(logo, logo_frame)
     
     pygame.display.update()
     
 #Event: the way your program interacts with the world (e.g. if someone presses a key in a game)
-    
