c9dda9fb70cfb3fa1032a4adf910510a260c7920
Xiangan He <xiangan@polychain.capital>
2022-04-27 14:48:29 -0400

    final project completed + more notes

--- Final/notes.py
+++ Final/notes.py
@@ -2,31 +2,72 @@
 
 import pygame
 
-pygame.init()
+class Card:
+    #constructor
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.faceup = True
+        self.held = False
 
-scene = pygame.display.set_mode((1400,800))
-pygame.display.set_caption("My new caption!")
+        front = pygame.image.load('./img/cards/' + rank + '_of_' + suit + '.png')
+        front = pygame.transform.smoothscale(front, (140, 200))
+        self.front = front
+        back = pygame.image.load('./img/back-side.png')
+        back = pygame.transform.smoothscale(back, (140, 200))
+        self.back = back
 
-pygame.mixer.music.load('music.mp3')
+        self.location = self.front.get_rect()
 
-pygame.mixer.music.play()
+    def stage(self, scene):
+        if self.faceup:
+            scene.blit(self.front, self.location)
+        else:
+            scene.blit(self.back, self.location)
 
-running = True
-
-logo = pygame.image.load('logo.png')
-logo_frame = logo.get_rect()
-
-while running:
+if __name__ == '__main__':
     
-    for event in pygame.event.get():
+    pygame.init()
+
+    scene = pygame.display.set_mode((1400,800))
+    pygame.display.set_caption("Test code for cards")
+    
+    cards = []
+
+    #pygame.mixer.music.load('music.mp3')
+
+    #pygame.mixer.music.play()
+
+    running = True
+
+    #logo = pygame.image.load('logo.png')
+    #logo_frame = logo.get_rect()
+
+    while running:
         
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    card = Card('diamonds', '10')
+                    cards.append(card)
+                elif event.key == pygame.K_c:
+                    card = Card('hearts', '8')
+                    cards.append(card)
 
-    
-    scene.fill([0,153,0])
-    scene.blit(logo, logo_frame)
-    
-    pygame.display.update()
+        
+        scene.fill([0,153,0])
+        for card in cards:
+            card.stage(scene)
+        pygame.display.update()
     
 #Event: the way your program interacts with the world (e.g. if someone presses a key in a game)
+
+#Object oriented programming
+#First determine what classes (aka. attributes) are for the object.
+
+#e.g. with a card, you have the classes: suit, rank, color, shape, front / back, print?
+
+#methods: flip card, draw card, stack card, move card to different pile
--- Final/reverseInteger.py
+++ Final/reverseInteger.py
@@ -1,9 +1,11 @@
-class Solution(object):
-    def reverse(self, x):
+#https://leetcode.com/problems/reverse-integer/
+class Solution:
+    def reverse(self, x: int) -> int:
         arr = list(str(x))
         n = len(arr)
         new_arr = [0] * n
         k = 0
+
         if arr[0] == '-':
             new_arr = [0] * (n-1)
             for i in range(n-1,0,-1):
@@ -23,5 +25,7 @@ class Solution(object):
                 a_string = "".join(ans)
 
                 res = int(a_string)
-        
-        return res
+        if res > pow(2,31) or res < pow(-2,31):
+            return 0
+        else:
+            return res
